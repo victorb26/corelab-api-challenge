87c08603c3476060972da9979c9c8ff3cd395c91
Victor Barros <victorjuda49@gmail.com>
2022-07-10 22:01:58 -0300

    Iniciando servidor básico para teste

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
-console.log('test')
+import express,{Request, Response} from  "express";
+
+const PORT = process.env.PORT || 5000;
+
+const app = express();
+
+app.get('/', (req:Request, res: Response)=>
+res.json({
+    msg:'Ok'
+}))
+
+app.listen (PORT, () => console.log("Servidor iniciado na porta" +  PORT));
